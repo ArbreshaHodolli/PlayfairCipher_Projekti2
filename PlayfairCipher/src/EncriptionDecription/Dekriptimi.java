48614a4b1c152ea5bbde7823474b8f6877265e36
Arbreshass <93888146+Arbreshass@users.noreply.github.com>
2022-04-17 14:49:55 +0200

    Update Dekriptimi.java

--- PlayfairCipher/src/EncriptionDecription/Dekriptimi.java
+++ PlayfairCipher/src/EncriptionDecription/Dekriptimi.java
@@ -1,5 +1,15 @@
-package EncriptionDecription;
+package Projekti_5G;
+import java.util.Scanner;
 
 public class Dekriptimi {
 
-}
+    static void decrypt(String encpt, String key) {
+        char[] encpt_arr = encpt.toCharArray();
+        char[] key_arr = key.toCharArray();
+        char[]
+                alpha = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
+                'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X'
+                , 'Y', 'Z'};
+        char[][] table = new char[5][5];
+        int ctr = -1;
+        
